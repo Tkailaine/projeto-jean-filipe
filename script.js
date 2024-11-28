07d6b816c57b86d0245d07e2ab28b42812997e81
Thaissa Kailaine <thaissakailaine.ti@gmail.com>
2024-11-28 17:33:56 -0300

    ajustes relatos

--- script.js
+++ script.js
@@ -157,25 +157,32 @@ const role = document.querySelector('.role')
 const testimonials = [
   
   {
-    name: 'Fábio Guedes',
+    name: 'Dinei Ferreira',
     position: '',
     photo: '',
     text:
-    "Dr. Jean me ajudou a garantir meus direitos e receber minha rescisão",
+    "Agradeço todo o desempenho e dedicação no meu caso, estou muito feliz também com o resultado! O seu profissionalismo é incontestável desde o início do processo até o fechamento do mesmo... Jean que Deus te abençoe grandemente e que você continue sempre esse profissional..Muito obrigado mesmo, sucesso!",
   },
   {
-    name: 'Alan André',
+    name: 'Vitória Silva',
     position: '',
     photo: '',
     text:
-    "Excelente advogado! ",
+    "Sou extremamente grata ao Dr. Jean, por me ajudar em um processo que eu não esperava, quando agendei uma reunião com ele percebi que fiz a escolha certa. Ele é um EXCELENTE advogado, um profissional comprometido com o trabalho. Nos atende sempre com seriedade e competência. Um trabalho sério e confiável. Sempre atencioso e esclarecendo todas as minhas dúvidas. Muito obrigado por tudo!",
   },
   {
-    name: 'Patrícia Linda Di Pietro',
+    name: 'Thaissa Oliveira',
     position: '',
     photo: '',
     text:
-    "Sou cliente do Dr. Jean desde 2018 e agradeço pelo apoio e presteza de sempre.",
+    "Recomendo o Dr. Jean por ser um advogado super competente no que faz, além disso, sua empatia com o cliente, dedicação e apoio foi o que mais me chamou a atenção. Ele atuou em um caso meu que fiquei desesperada e logo após que ele tomou à frente, soube que estava em boas mãos. Obrigada Jean! ",
+  },
+  {
+    name: 'Talita Ferreira',
+    position: '',
+    photo: '',
+    text:
+    "Gostaria de expressar minha eterna gratidão pelo excelente trabalho realizado. Toda sua dedicação, seu profissionalismo e competência fizeram toda a diferença em um momento delicado. Sua atuação impecável fez toda a diferença, e não tenho palavras para agradecer por todo o empenho e cuidado com que tratou o caso. Você não é apenas um ótimo advogado, mas uma pessoa de caráter, que demonstrou compromisso e com melhor interesse com seu cliente. Agradeço imensamente por tudo que fez, e fico tranquila sabendo que pude contar com você que é capacitado e confiável. Minha eterna gratidão e respeito!",
   }
 ]
 
@@ -206,7 +213,7 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             title: "Décimo terceiro adiantado: empresas têm até sexta-feira (29) para pagar a 1ª parcela",
             date: "25/11/2024",
-            image: "./imagens/decimo.JPG",
+            image: "imagens/decimo.jpg",
             summary: "As empresas têm até sexta-feira (29) para pagar a 1ª parcela do 13º salário, garantindo o direito dos trabalhadores.",
             content: "As empresas têm até sexta-feira (29) para pagar a primeira parcela do 13º salário, conforme determina a lei. O prazo original é 30 de novembro, mas foi antecipado devido ao último dia útil. O valor corresponde a metade do salário bruto de novembro, sem descontos, enquanto a segunda parcela, com deduções de IR e INSS, será paga até 20 de dezembro.O 13º é garantido por lei aos trabalhadores CLT e pode ser proporcional ao tempo de serviço. Caso o prazo final coincida com domingos ou feriados, o pagamento deverá ser antecipado. Segundo o Dieese, o benefício deve injetar R$ 321,4 bilhões na economia este ano",
             source: "Fonte: CNN Brasil"
@@ -215,7 +222,7 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             title: "Justiça do Trabalho garante adicional de insalubridade a camareira de motel",
             date: "23/11/2024",
-            image: "./imagens/camareira.JPG",
+            image: "imagens/camareira.jpg",
             summary: "Justiça do Trabalho garante adicional de insalubridade a camareira de motel. Saiba mais sobre a decisão!",
             content: "A Justiça do Trabalho condenou um motel em Sobral (CE) a pagar à camareira o adicional de insalubridade em grau máximo, devido a riscos à saúde no ambiente de trabalho, como contato com resíduos biológicos e falta de EPIs adequados. Além disso, a empresa deverá pagar reflexos sobre 13º, férias, FGTS e outros encargos, após análise de perícia técnica.",
             source: "Fonte: Consultor Jurídico"
@@ -224,7 +231,7 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             title: "10 Estratégias para Evitar Processos Trabalhistas e Promover um Ambiente de Trabalho Saudável",
             date: "03/08/2024",
-            image: "./imagens/equipe.JPG",
+            image: "imagens/equipe.jpg",
             summary: "Descubra 10 dicas essenciais para evitar processos trabalhistas e criar um ambiente de trabalho mais saudável e produtivo.",
             content: "Manter um negócio sem enfrentar processos trabalhistas exigem estratégias estratégicas, como políticas claras, compliance trabalhista, boas práticas de gestão de RH e treinamentos contínuos. Além disso, prevenir o assédio, promover uma comunicação aberta e investir em auditorias internacionais ajuda a reduzir riscos. Consultoria jurídica preventiva e um ambiente saudável também são fundamentais para evitar litígios e garantir o bem-estar dos colaboradores. Implementando essas ações, sua empresa estará mais protegida legalmente e promoverá um local de trabalho mais harmonioso.",
             source: "Fonte: Migalhas de Peso"
@@ -234,7 +241,7 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             title: " Demitida Durante Contrato de Experiência, Gestante Deve Receber Salários e Indenização por Estabilidade",
             date: "10/08/2024",
-            image: "./imagens/gestante.JPG",
+            image: "imagens/gestante.jpg",
             summary: "Gestante demitida durante o contrato de experiência tem direito a contratação, benefícios e indenização. Descubra os detalhes da decisão judicial!",
             content: "A 5ª Turma do Tribunal Regional do Trabalho da 4ª Região (RS) determinou que uma gestante demitida durante o contrato de experiência receba a cláusula contratual correspondente ao período de estabilidade, indenização por danos morais de R$ 10 mil, além de 13º salário, férias, FGTS e multa de 40%. A empresa foi condenada por discriminar uma trabalhadora, que já estava ciente do risco de gestação, configurando violação dos direitos da gestante e da dignidade humana. A decisão foi unânime entre os desembargadores.",
             source: "Fonte: Justiça do Trabalho"
@@ -242,7 +249,7 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             title: " Por que as Startups Precisam de um Programa de Compliance?",
             date: "22/08/2024",
-            image: "./imagens/startup.JPG",
+            image: "imagens/startup.jpg",
             summary: "Saiba por que as startups devem adotar um programa de compliance desde o início para garantir ética, segurança e atrair investidores. Entenda os benefícios!",
             content: "Para garantir a inovação e o crescimento sustentável, as startups devem adotar práticas de compliance, que garantam a conformidade com leis e padrões éticos. Isso envolve desde a segurança da informação até a prevenção à lavagem de dinheiro e corrupção. Um programa de compliance eficaz não apenas reduz riscos e protege a confiança, como também é essencial para atrair investimentos. Ao incorporar essas práticas, as startups constroem uma base sólida, demonstrando compromisso com a ética e a transparência, o que é crucial para seu sucesso a longo prazo.",
             source: "Fonte: Exame."
@@ -250,7 +257,7 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             title: "Juiz Declara Rescisão Indireta de Trabalhadora Endividada por Atrasos de Salários",
             date: "29/05/2024",
-            image: "./imagens/salario.JPG",
+            image: "imagens/salario.jpg",
             summary: "Justiça permite rescisão indireta de contrato por atrasos salariais e inadimplência no FGTS, com indenização de R$ 5 mil.",
             content: "A Justiça do acompanhado Trabalho a rescisão indireta do contrato de um trabalhador de uma instituição educacional de Caratinga, após repetidos atrasos salariais e inadimplência no FGTS. A empregada, que ficou individualizada e teve seu nome incluído no SPC, foi indenizada em R$ 5 mil por danos morais. O juiz entendeu que o descumprimento das obrigações trabalhistas, incluindo o pagamento tardio dos contratos, causou grave prejuízo moral à funcionária. A empresa recorreu, mas a decisão foi mantida pelo Tribunal Regional do Trabalho.",
             source: "Fonte: Justiça do Trabalho"
@@ -342,3 +349,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
     renderPosts(); // Renderiza os primeiros posts ao carregar a página
 });
+
+
